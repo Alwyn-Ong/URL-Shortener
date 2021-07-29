840172e574d3c48b0961f4d819a64247eaf6c6d6
Alwyn-Ong <alwyn.ong.2018@sis.smu.edu.sg>
2021-07-29 22:47:21 +0800

    Separate animated text into separate component, centralised content

--- frontend/src/components/TypistLoop.js
+++ frontend/src/components/TypistLoop.js
@@ -1,26 +1,38 @@
+import { Typography } from "@material-ui/core";
 import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom";
 import Typist from "react-typist";
+import "react-typist/dist/Typist.css";
+import "./styles.css";
 
 export default function TypistLoop(first, second) {
   const [count, setCount] = useState(1);
 
   console.log(count);
   useEffect(() => {
-    setCount(1)
+    setCount(1);
     // setCount(1);
   }, [count]);
 
+  let original = "https://developer.gov.sg/";
+  let shortened = `http://${process.env.REACT_APP_SERVER_NAME}/a1b2c3d4`
+  console.log(process.env.REACT_APP_SERVER_NAME);
+
   return (
-    <div>
+    <div className="animated-text">
       {count ? (
-        <Typist startDelay={1000} delay={1000} onTypingDone={() => setTimeout(() => setCount(0), 2000)}>
-          <span> www.abc.com/defghijklmno/pqrs</span>
-          <Typist.Backspace count={"defghijklmno/pqrs".length} delay={800} />
-          <span>abc123</span>
+        <Typist
+          startDelay={1000}
+          delay={1000}
+          onTypingDone={() => setTimeout(() => setCount(0), 2000)}
+        >
+          <span>{original}</span>
+          <Typist.Backspace count={original.length} delay={2000} />
+          <span>{shortened}</span>
         </Typist>
       ) : (
-        <span><br/></span>
+        <span>
+          <br />
+        </span>
       )}
     </div>
   );
---
+++ frontend/src/views/Main.js
@@ -0,0 +1,107 @@
+import {
+  Container,
+  Grid,
+  Typography,
+  TextField,
+  Button,
+  Box,
+} from "@material-ui/core";
+import React from "react";
+import TypistLoop from "../components/TypistLoop";
+import { makeStyles } from "@material-ui/styles";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    height: "80vh",
+  },
+}));
+
+export const Main = () => {
+  const classes = useStyles();
+
+  const [url, setUrl] = React.useState("");
+  const handleChange = (event) => {
+    setUrl(event.target.value);
+    console.log(url);
+  };
+
+  const handleSubmit = () => {
+    if (url.length == 0) {
+      setHelperText("Please enter a URL");
+      setIsValid(false);
+      return;
+    }
+    alert(`${url} submitted!`);
+  };
+
+  const [helperText, setHelperText] = React.useState("");
+  const [isValid, setIsValid] = React.useState(true);
+
+  return (
+    <div className={classes.root}>
+      <Container maxWidth="lg">
+        <Box
+          display="flex"
+          flex="1"
+          justifyContent="space-around"
+          style={{ height: "100vh" }}
+        >
+          <Grid
+            container
+            direction="column"
+            alignItems="center"
+            justifyContent="center"
+            spacing={5}
+          >
+            <Grid item>
+              <Typography variant="h1" align="center">
+                {" "}
+                URL Shortener
+              </Typography>
+            </Grid>
+            <Grid>
+              <TypistLoop />
+            </Grid>
+            <Grid item>
+              <Typography variant="h2" align="center">
+                Less is more.
+              </Typography>
+              {/* <Typography variant="h3" align="center">Enter your URL to get started:</Typography> */}
+            </Grid>
+            <Grid item style={{ width: "50%" }}>
+              <Typography variant="h5" align="center">
+                Enter your URL to get started!
+              </Typography>
+              <TextField
+                id="standard-full-width"
+                // label={
+                //   <Typography variant="h7">
+                //     Enter your URL to get Started:
+                //   </Typography>
+                // }
+                label="URL"
+                style={{ margin: 8, height: "50%" }}
+                placeholder="www."
+                helperText={!isValid && helperText}
+                fullWidth
+                // margin="normal"
+                // InputLabelProps={{
+                //   shrink: true,
+                // }}
+                value={url}
+                onChange={handleChange}
+                error={!isValid}
+                variant="outlined"
+              />
+            </Grid>
+            <Grid item>
+              <Button variant="contained" onClick={handleSubmit}>
+                Generate
+              </Button>
+            </Grid>
+          </Grid>
+        </Box>
+      </Container>
+    </div>
+  );
+};
